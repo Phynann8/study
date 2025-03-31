4cd1e1fc41c205baab876ecd37fa9ee240653d49
Chhun Phynann <phynann8@gmail.com>
2025-03-31 15:32:46 +0700

    add page underdev

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.get('/contact', (req, res) => {
 app.get('/csharp-programming', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'csharp-lesson.html'));
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
